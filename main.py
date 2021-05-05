5c7deb121c94c481657d9e2fc1fef85c5d3c43f7
jxu <7989982+jxu@users.noreply.github.com>
2021-05-05 02:14:01 -0400

    Add diamond erasing region

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ print('==> Preparing data..')
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
-    RandomErasingTransform(mode="ellipse"),
+    RandomErasingTransform(mode="diamond"),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
